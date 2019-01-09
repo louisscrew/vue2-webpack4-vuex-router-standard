8d4caf984140d03fc8dc0ae93b1053b5e86b2481
louis wen <75168611@qq.com>
2019-01-09 18:40:09 +0800

    Add files via upload

--- template/build/webpack.base.config.js
+++ template/build/webpack.base.config.js
@@ -1,140 +1,85 @@
-const path  = require('path');
-const config = require("./config.js");
 const webpack = require('webpack');
+const path = require('path');
+const config = require('../config');
+// vue-loader 插件
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+// html插件
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// const CopyWebpackPlugin = require('copy-webpack-plugin');+
-//VueLoader 在15版本之后需要应用这个插件在webpack中才能正常解析vue文件
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// 使用happypack
+const HappyPack = require('happypack');
+const os = require('os');
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+
+function resolve(dir) {
+	return path.join(__dirname, '..', dir)
 }
 
-
-const webpackConfig = {
+module.exports = {
+    context: path.resolve(__dirname, '../'),
     entry: {
-        main: ['babel-polyfill','./src/main.js']
-    },
-    output:{
-        path:config.webpack.outputPath, 
-        filename: '[name].[hash:8].js',
-        libraryTarget: 'umd',
-    },
-    resolve:{
-        modules: [
-            "node_modules"
+		build: ['babel-polyfill','./src/index.js']
+	},
+    output: {
+		path: config.build.assetsRoot,
+		filename: '[name].js',
+		publicPath: config.dev.assetsPublicPath
+	},
+    resolve: {
+		modules: [
+			"node_modules",
+			resolve('src')
+		],
+		alias: {
+			'vue$': 'vue/dist/vue.esm.js',
+			'@': resolve('src'),
+			'jquery': 'lib/jquery/jquery.min.js',
+			'webuploader': 'lib/uploader/webuploader.js'//上传插件
+		},
+		extensions: ['.js', '.vue', '.json']
+	},
+	module: {
+        rules: [
+            {
+                test: /\.js$/,
+                //把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
+                loader: 'happypack/loader?id=happyBabel',
+                //排除node_modules 目录下的文件
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+            },
         ],
-        alias: {
-            'vue$': 'vue/dist/vue.common.js',
-            '@': resolve('src')
-        },
-        extensions:[".js",".json",".jsx",".css",'.vue']
     },
-    module:{
-        rules:[
-            {
-                test:/\.js$/,
-                exclude:/(node_modules|bower_components)/,
-                // include: [resolve('src'), resolve('test')],
-                // enforce: 'pre',
-                use:[{
-                    loader:'babel-loader'
-                }]
-            },
-            {
-                test:/\.vue$/
-                ,use:{
-                    loader: 'vue-loader',
-                    options: {
-                        loaders: {
-                            'less': 'vue-style-loader!css-loader!less-loader'
-                        }
-                    }
-                }
-            },
-            {
-                test:/\.css$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:'style-loader', // 回滚
-                    publicPath:'../', //解决css背景图的路径问题
-                    use:[{
-                        loader: 'css-loader',
-                        options: {
-                            autoprefixer: false
-                        }
-                    },{
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }]
-                })
-            },
-            {
-                test:/\.less$/,
-                use:ExtractTextPlugin.extract({ //分离less编译后的css文件
-                    fallback:'style-loader',
-                    publicPath:'../', //解决css背景图的路径问题
-                    use:[{
-                        loader: 'css-loader',
-                        options: {
-                            autoprefixer: false
-                        }
-                    },{
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },{
-                        loader: 'less-loader'
-                    }]
-                })
-            },
-            {
-                test:/\.(sass|scss)$/,
-                use:ExtractTextPlugin.extract({
-                    fallback:'style-loader',
-                    publicPath:'../', //解决css背景图的路径问题
-                    use:[{
-                        loader: 'css-loader',
-                        options: {
-                            autoprefixer: false
-                        }
-                    },{
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },{
-                        loader: 'sass-loader'
-                    }]
-                })
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2|svgz)$/,
-                use:{
-                    loader: 'file-loader',
-                    options: {
-                      name: 'assets/images/[name].[ext]?[hash]'
-                    }
-                }
-            },
-        ]
-           
-    },
-    performance: {
-        hints: false
-    },
-    plugins:[
+    plugins: [
         new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            template:'./src/index.html',
-            inject: true // true->'head' || false->'body'
-        }),
-        new ExtractTextPlugin('./css/[name].css')
-    ]
+		new HappyPack({
+			//用id来标识 happypack处理那里类文件
+			id: 'happyBabel',
+			//如何处理  用法和loader 的配置一样
+			loaders: [{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				include: [resolve('src'), resolve('node_modules/webpack-dev-server/client')]
+			}],
+			//共享进程池
+			threadPool: happyThreadPool,
+			//允许 HappyPack 输出日志
+			verbose: true,
+		}),
+        // 解决vender后面的hash每次都改变
+        new webpack.HashedModuleIdsPlugin(),
+		//处理html
+		new HtmlWebpackPlugin({
+			filename: config.build.index,
+			template: './src/index.html'
+		})
+	]
 }
-
-module.exports = webpackConfig;
