7e878de8c56866573edc2b201bf72993fb855ad4
louis wen <75168611@qq.com>
2019-01-09 18:38:59 +0800

    Update postcss.config.js

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,17 +1,6 @@
+// CSS后处理
 module.exports = {
-    plugins: [
-        //自动添加索引
-        require('autoprefixer')({
-            browsers: ['last 2 versions', 'ie >= 9', '> 5% in CN']
-        }),
-        //压缩样式
-        require('cssnano')({
-            // 关闭cssnano的autoprefixer选项，不然会和前面的autoprefixer冲突
-            autoprefixer: false,
-            reduceIdents: false,
-            zindex: false,
-            discardUnused: false,
-            mergeIdents: false
-        })
-    ]
-};
+  plugins: [
+    require('autoprefixer')
+  ]
+}
