c5b08f3f87ae8ebfa81010c29eec1c121ccef7f8
louis wen <75168611@qq.com>
2019-01-09 18:42:25 +0800

    Add files via upload

---
+++ template/src/App.vue
@@ -0,0 +1,16 @@
+<style scoped lang="less">
+
+</style>
+<template>
+    <div id="app">
+        hello world
+    </div>
+</template>
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {};
+    }
+};
+</script>
