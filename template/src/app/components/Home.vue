c0103f89b4e81965565d6a959854e031b83c4af6
louis wen <75168611@qq.com>
2019-08-21 14:34:32 +0800

    Create Home.vue

---
+++ template/src/app/components/Home.vue
@@ -0,0 +1,30 @@
+<style lang="less" scoped>
+
+.home-cust-css{
+    div.home-color{
+        color: #ff0000;
+    }
+}
+
+</style>
+<template>
+    <div class="home-cust-css">
+        <div class="home-color">{{ msg }}</div>
+    </div>
+</template>
+<script>
+export default {
+    name: "Home",
+    data () {
+        return {
+            msg:"hello Home"
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+        
+    }
+}
+</script>
