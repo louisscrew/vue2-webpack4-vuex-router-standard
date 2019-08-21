97ee558cf24bb0d68a47210103c9f08cce67c3f7
louis wen <75168611@qq.com>
2019-08-21 14:20:35 +0800

    Add files via upload

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,6 +1,13 @@
-// CSS后处理
+
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
+    plugins: [
+        require('postcss-import'),
+        require('postcss-url'),
+        require('postcss-cssnext'),
+        // add your "plugins" here
+        // ...
+        // and if you want to compress
+        // require('cssnano'),
+        require('postcss-browser-reporter')
+    ]
 }
